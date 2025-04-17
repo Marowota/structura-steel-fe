5f7a432f47a7058aea69f9d3279567a23f358ce3
Marowota <57163635+Marowota@users.noreply.github.com>
2025-04-17 21:51:08 +0700

    fix layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
 }>) {
   return (
     <div className="bg-white-200 flex h-screen w-full">
-      <div className="group flex w-20 flex-col items-center bg-white px-3 py-2 shadow-xl transition-all duration-300 hover:w-72">
+      <div className="group flex w-20 flex-col items-center overflow-hidden bg-white px-3 py-2 text-nowrap shadow-xl transition-all duration-300 select-none hover:w-72">
         <div className="flex min-h-28 w-full items-center justify-center">
           <Image
             src="/structura-steel-icon.png"
@@ -25,6 +25,12 @@ export default function MainLayout({
         <Button variant={"navbarSelected"} size="nav" className="">
           Lorem ipsum
         </Button>
+        <div className="mt-auto flex min-h-10 w-full items-center gap-4 border-t-[1px] border-gray-500 px-2 py-2">
+          <div className="h-10 min-w-10 rounded-full bg-gray-500"></div>
+          <div className="text-md-semibold flex w-full flex-col overflow-hidden">
+            <div>Jonny Adnrew</div>
+          </div>
+        </div>
       </div>
       {children}
     </div>
