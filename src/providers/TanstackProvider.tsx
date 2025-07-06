b27178e757e53075b2d6cb5a9a45f23b451495b3
Marowota <57163635+Marowota@users.noreply.github.com>
2025-07-06 10:33:32 +0700

    fix tanstack doesnt update query on build

--- src/providers/TanstackProvider.tsx
+++ src/providers/TanstackProvider.tsx
@@ -2,17 +2,22 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
 export const TanstackProvider = ({
   children,
 }: {
   children?: React.ReactNode;
 }) => {
-  const queryClient = new QueryClient();
+  // Create QueryClient instance only once using useState with lazy initialization
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 };
